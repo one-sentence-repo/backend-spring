01a82a2ebfe510c56c57f2c9400aec3b287fd4f3
예비 리액트맨 <70879978+kidboi666@users.noreply.github.com>
2024-12-11 13:56:36 +0900

    first commit

---
+++ src/main/java/one/sentence_spring/SentenceSpringApplication.java
@@ -0,0 +1,13 @@
+package one.sentence_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentenceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentenceSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/sentence_spring/SentenceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package one.sentence_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentenceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
